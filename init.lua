37b65f67b1af2757470c6568195b715d9e0e3bfe
Sondre <31799749+sonlar@users.noreply.github.com>
2024-04-26 16:57:15 +0200

    Fix spacing with LuaSnip

--- init.lua
+++ init.lua
@@ -669,10 +669,10 @@ require('lazy').setup({
       -- Snippet Engine & its associated nvim-cmp source
       {
         'L3MON4D3/LuaSnip',
-	      -- follow latest release.
-	      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	      -- install jsregexp (optional!).
-	      build = "make install_jsregexp",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp",
         dependencies = {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
