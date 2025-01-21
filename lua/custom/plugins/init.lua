e23bcb3efb229712ee819c0e00ca139f6beef984
sonlar <z5bewil7@anonaddy.me>
2025-01-21 21:53:49 +0100

    add linting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'brianhuster/live-preview.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+  },
+}
