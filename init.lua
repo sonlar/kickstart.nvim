19b1cd4da46433e3e9a4215f2e41118d3f5a05d7
sonlar <z5bewil7@anonaddy.me>
2025-02-18 12:09:06 +0100

    Switch js LSP eslint -> ts_ls & uncomment debug + autopairs plugin

--- init.lua
+++ init.lua
@@ -626,8 +626,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
+        -- eslint = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -931,10 +932,10 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
