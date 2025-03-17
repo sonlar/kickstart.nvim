e9e29a7fa33a3a1155431cf31f64b9ad681ba525
sonlar <31799749+sonlar@users.noreply.github.com>
2025-03-17 11:51:35 +0100

    Change structure of custom plugins + add nvim-surround

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,11 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-    'brianhuster/live-preview.nvim',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-    },
-  },
-}
+return {}
