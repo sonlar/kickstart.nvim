db3584d884d864d6455883e64a39c32f90b03906
sonlar <31799749+sonlar@users.noreply.github.com>
2025-11-29 14:38:06 +0100

    Add bandit, black, isort installation

--- init.lua
+++ init.lua
@@ -591,6 +591,9 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        bandit = {},
+        black = {},
+        isort = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
